cb284eb37e1c825e817a7fc6959614efb261a154
Ramy-Hedhli <Rmhedhly@gmail.com>
2022-09-28 20:00:36 +0100

    versionFinal

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+//déclaration des variable
 var art = document.querySelectorAll(".product");
 var plus = document.querySelectorAll(".qt-plus");
 var moin = document.querySelectorAll(".qt-minus");
-//var qte = document.querySelectorAll(".qt");
-//var unitprice = document.querySelectorAll(".price");
-//var artprice = document.querySelectorAll("full-price");
-//var stt = document.querySelector("#sstotal");
-//var total = document.querySelector("#total");
 var coeur = document.querySelectorAll(".favori");
 var suprim = document.querySelectorAll(".remove");
-
-//function soustotal(a, b) {
-//for (let i = 0; i < art.length; i++) {
-//artprice[i] = a[i].innerText * b[i].innerText;
-// }
-//}
+//prix de l'article en fonction de la quantité
+function soustotal() {
+  var qte = document.querySelectorAll(".qt");
+  var unitprice = document.querySelectorAll(".price");
+  var artprice = document.querySelectorAll(".full-price");
+  for (let i = 0; i < art.length; i++) {
+    artprice[i].innerText = +unitprice[i].innerText * +qte[i].innerText;
+    console.log(artprice[i]);
+  }
+}
+//prix total
 function somme() {
   var qte = document.querySelectorAll(".qt");
   var unitprice = document.querySelectorAll(".price");
@@ -27,29 +28,23 @@ function somme() {
 
   total.innerHTML = sum;
 }
-
+//exécution
 for (let i = 0; i < art.length; i++) {
   plus[i].addEventListener("click", function () {
     plus[i].previousElementSibling.innerText++;
-    //soustotal(qte, unitprice);
+    soustotal();
     somme();
   });
   moin[i].addEventListener("click", function () {
     if (moin[i].nextElementSibling.innerText > 0) {
       moin[i].nextElementSibling.innerText--;
-      //soustotal(qte, unitprice);
+      soustotal();
       somme();
     }
   });
-}
-
-for (let i = 0; i < coeur.length; i++) {
   coeur[i].addEventListener("click", function () {
     coeur[i].classList.toggle("Rouge");
   });
-}
-
-for (let i = 0; i < art.length; i++) {
   suprim[i].addEventListener("click", function () {
     suprim[i].parentElement.parentElement.remove();
     somme();
