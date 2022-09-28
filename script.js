6c601a78036e4222e0f86fb5bb052e1b908aaeae
Ramy-Hedhli <Rmhedhly@gmail.com>
2022-09-28 17:45:45 +0100

    VF

--- script.js
+++ script.js
@@ -1,95 +1,57 @@
-var compt1 = 1;
-var p1 = document.getElementById("price1").innerText;
-document.getElementById("qt-plus1").addEventListener("click", function () {
-  compt1 = compt1 + 1;
-  document.getElementById("qt1").innerText = compt1;
-  var t1 = (document.getElementById("full-price1").innerText = +p1 * compt1);
-  return t1;
-});
+var art = document.querySelectorAll(".product");
+var plus = document.querySelectorAll(".qt-plus");
+var moin = document.querySelectorAll(".qt-minus");
+//var qte = document.querySelectorAll(".qt");
+//var unitprice = document.querySelectorAll(".price");
+//var artprice = document.querySelectorAll("full-price");
+//var stt = document.querySelector("#sstotal");
+//var total = document.querySelector("#total");
+var coeur = document.querySelectorAll(".favori");
+var suprim = document.querySelectorAll(".remove");
 
-document.getElementById("qt-minus1").addEventListener("click", function () {
-  if (compt1 > 0) {
-    compt1 = compt1 - 1;
-    document.getElementById("qt1").innerText = compt1;
-    var t1 = (document.getElementById("full-price1").innerText = +p1 * compt1);
-    return t1;
+//function soustotal(a, b) {
+//for (let i = 0; i < art.length; i++) {
+//artprice[i] = a[i].innerText * b[i].innerText;
+// }
+//}
+function somme() {
+  var qte = document.querySelectorAll(".qt");
+  var unitprice = document.querySelectorAll(".price");
+  var total = document.querySelector("#total");
+
+  var sum = 0;
+
+  for (let i = 0; i < unitprice.length; i++) {
+    sum = sum + unitprice[i].innerHTML * qte[i].innerHTML;
   }
-});
 
-//
+  total.innerHTML = sum;
+}
 
-var compt2 = 1;
-var p2 = document.getElementById("price2").innerText;
-document.getElementById("qt-plus2").addEventListener("click", function () {
-  compt2 = compt2 + 1;
-  document.getElementById("qt2").innerText = compt2;
-  var t2 = (document.getElementById("full-price2").innerText = +p2 * compt2);
-  return t2;
-});
+for (let i = 0; i < art.length; i++) {
+  plus[i].addEventListener("click", function () {
+    plus[i].previousElementSibling.innerText++;
+    //soustotal(qte, unitprice);
+    somme();
+  });
+  moin[i].addEventListener("click", function () {
+    if (moin[i].nextElementSibling.innerText > 0) {
+      moin[i].nextElementSibling.innerText--;
+      //soustotal(qte, unitprice);
+      somme();
+    }
+  });
+}
 
-document.getElementById("qt-minus2").addEventListener("click", function () {
-  if (compt2 > 0) {
-    compt2 = compt2 - 1;
-    document.getElementById("qt2").innerText = compt2;
-    var t2 = (document.getElementById("full-price2").innerText = +p2 * compt2);
-    return t2;
-  }
-});
+for (let i = 0; i < coeur.length; i++) {
+  coeur[i].addEventListener("click", function () {
+    coeur[i].classList.toggle("Rouge");
+  });
+}
 
-//
-
-var compt3 = 1;
-var p3 = document.getElementById("price3").innerText;
-document.getElementById("qt-plus3").addEventListener("click", function () {
-  compt3 = compt3 + 1;
-  document.getElementById("qt3").innerText = compt3;
-  var t3 = (document.getElementById("full-price3").innerText = +p3 * compt3);
-  return t3;
-});
-
-document.getElementById("qt-minus3").addEventListener("click", function () {
-  if (compt3 > 0) {
-    compt3 = compt3 - 1;
-    document.getElementById("qt3").innerText = compt3;
-    var t3 = (document.getElementById("full-price3").innerText = +p3 * compt3);
-    return t3;
-  }
-});
-
-//document.getElementById("sstotal").innerText = t1 + t2 + t3;
-
-document.getElementById("heart1").addEventListener("click", function () {
-  if (document.getElementById("heart1").style.backgroundColor != "red") {
-    document.getElementById("heart1").style.backgroundColor = "red";
-  } else {
-    document.getElementById("heart1").style.backgroundColor = "white";
-  }
-});
-
-document.getElementById("heart2").addEventListener("click", function () {
-  if (document.getElementById("heart2").style.background != "red") {
-    document.getElementById("heart2").style.background = "red";
-  } else {
-    document.getElementById("heart2").style.background = "white";
-  }
-});
-
-document.getElementById("heart3").addEventListener("click", function () {
-  if (document.getElementById("heart3").style.background != "red") {
-    document.getElementById("heart3").style.background = "red";
-  } else {
-    document.getElementById("heart3").style.background = "white";
-  }
-});
-
-document.getElementById("remove1").addEventListener("click", function () {
-  document.getElementById("article1").remove();
-});
-
-document.getElementById("remove2").addEventListener("click", function () {
-  document.getElementById("article2").remove();
-});
-
-document.getElementById("remove3").addEventListener("click", function () {
-  document.getElementById("article3").remove();
-});
+for (let i = 0; i < art.length; i++) {
+  suprim[i].addEventListener("click", function () {
+    suprim[i].parentElement.parentElement.remove();
+    somme();
+  });
+}
